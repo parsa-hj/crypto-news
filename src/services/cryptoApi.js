efa0be5574959a6299a680701a5f484b11d75be7
Parsa Hajiha <hajihaparsa2004@gmail.com>
2022-10-24 23:16:20 -0500

    fetching api 1

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,19 @@
+
+
+// const options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/coins',
+//     params: {
+//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//       timePeriod: '24h',
+//       'tiers[0]': '1',
+//       orderBy: 'marketCap',
+//       orderDirection: 'desc',
+//       limit: '50',
+//       offset: '0'
+//     },
+//     headers: {
+//       'X-RapidAPI-Key': 'afae5cbeacmsh043a56b9839783dp130662jsn19191a24b88a',
+//       'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+//     }
+//   };
