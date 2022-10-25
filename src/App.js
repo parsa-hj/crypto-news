519f977c9f1d79343f4e0f7b9fbb8fa1301d02a9
Parsa Hajiha <hajihaparsa2004@gmail.com>
2022-10-24 20:42:21 -0500

    Basic layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import {Switch, Route, Link} from 'react-router-dom';
+import {Routes, Route, Link} from 'react-router-dom';
 import {Layout, Typography, Space} from 'antd';
 
-import { Navbar } from './components';
+import { Navbar, Exchanges, Cryptocurrencies, News, Homepage, CryptoDetails } from './components';
 import './App.css'
 
 const App = () => {
@@ -13,10 +13,34 @@ const App = () => {
         </div>
 
         <div className='main'>
+          <Layout>
+            <div className='routes'>
+              <Routes>
+
+                <Route path='/' element={<Homepage/>} />
+                <Route path='/exchanges' element={<Exchanges/>} />
+                <Route path='/cryptocurrencies' element={<Cryptocurrencies/>} />
+                <Route path='/crypto/:coinId' element={<CryptoDetails/>} />
+                <Route path='/news' element={<News/>} />
+
+              </Routes>
+            </div>
+          </Layout>
+        
+
+          <div className='footer'>
+            <Typography.Title level={5} style={{color: 'White', textAlign: 'center'}}>
+              Crypto News <br />
+              All rights reserved
+            </Typography.Title>
+            <Space>
+              <Link to="/">Home</Link>
+              <Link to="/exchanges">Exchanges</Link>s
+              <Link to="/news">News</Link>
+            </Space>
+          </div>
         </div>
 
-        <div className='footer'>
-        </div>
     </div>
   )
 }
