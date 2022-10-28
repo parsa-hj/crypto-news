8e967c9facd21e8dd95330b871a8a6b3ee0c3c59
Parsa Hajiha <hajihaparsa2004@gmail.com>
2022-10-27 23:11:57 -0500

    cryptoAPi created

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,10 +3,13 @@ import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
+import { useGetCryptosQuerry } from '../services/cryptoApi';
+
 const { Title } = Typography;
 
 
 const Homepage = () => {
+  const { data, isFetching } = useGetCryptosQuerry();
   return (
     <>
       <Title level={2} className="heading">Global Crypto Stats</Title>
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,3 +1,27 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Key': 'afae5cbeacmsh043a56b9839783dp130662jsn19191a24b88a',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com/coins';
+
+const createRequest = (url) => ({ url, headers: crutpApiHeaders })
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuerry,
+} = cryptoApi;
 
 
 // const options = {
