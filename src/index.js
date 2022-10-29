d67c2b8082aa93c64f53745cda1bbfd28a818b2d
Parsa Hajiha <hajihaparsa2004@gmail.com>
2022-10-29 00:29:11 -0500

    change

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { createRoot } from 'react-dom/client';
 
 import App from './App';
 import store from './app/store';
 import 'antd/dist/antd.css';
 
-ReactDOM.render(
+createRoot(
     
     <Router>
       <Provider store={store}>
